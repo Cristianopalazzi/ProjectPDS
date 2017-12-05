1eb8a3c1801025445cf8d91c187dd53a40481e40
Gianmaria <gianmaria.tremigliozzi@gmail.com>
2017-12-05 18:29:40 +0100

    Cambio Assembly

--- ProjectPDSWPF/ProjectPDSWPF/Properties/AssemblyInfo.cs
+++ ProjectPDSWPF/ProjectPDSWPF/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // Le informazioni generali relative a un assembly sono controllate dal seguente 
 // set di attributi. Modificare i valori di questi attributi per modificare le informazioni
 // associate a un assembly.
-[assembly: AssemblyTitle("ProjectPDSWPF")]
+[assembly: AssemblyTitle("EasyShare")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ProjectPDSWPF")]
+[assembly: AssemblyProduct("EasyShare")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- ProjectPDSWPF/ProjectPDSWPF/Properties/Resources.Designer.cs
+++ ProjectPDSWPF/ProjectPDSWPF/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace ProjectPDSWPF.Properties {
+namespace EasyShare.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace ProjectPDSWPF.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ProjectPDSWPF.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("EasyShare.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
